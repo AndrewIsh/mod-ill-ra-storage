9bd0f58f22269430d6298473e168bf0dce694933
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-06-07 10:37:39 +0100

    Add database setup scripts
    Add the required scripts and SQL to create and destroy the database and
    associated objects

---
+++ database-setup/create-schema.sql
@@ -0,0 +1,11 @@
+\c database_name
+
+CREATE SCHEMA schema_name AUTHORIZATION user_name;
+
+CREATE TABLE schema_name.ill_request (_id SERIAL PRIMARY KEY, jsonb JSONB NOT NULL);
+GRANT ALL ON schema_name.ill_request TO user_name;
+GRANT ALL ON schema_name.ill_request__id_seq TO user_name;
+
+CREATE TABLE schema_name.ill_request_status (_id SERIAL PRIMARY KEY, jsonb JSONB NOT NULL);
+GRANT ALL ON schema_name.ill_request_status TO user_name;
+GRANT ALL ON schema_name.ill_request__id_seq TO user_name;
