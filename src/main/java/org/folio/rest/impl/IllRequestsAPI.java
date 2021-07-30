8c20728d3f770e17f83fad5ecbe43b2831cd53e8
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-07-30 15:05:49 +0100

    Provide Submissions / Requests distinction
    This commit provides the distinction between a submission being made to
    a library and a subsequent request being made to a supplier. It
    includes:
    
    - Distinct submissions and requests endpoints
    - Submission statuses are stored and joined to submissions
    - Requests are joined to their corresponding submission

--- src/main/java/org/folio/rest/impl/IllRequestsAPI.java
+++ src/main/java/org/folio/rest/impl/IllRequestsAPI.java
@@ -2,197 +2,108 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import org.folio.rest.annotations.Validate;
-import org.folio.rest.jaxrs.model.IllStatus;
-import org.folio.rest.jaxrs.model.IllStatuses;
-import org.folio.rest.jaxrs.model.Request;
-import org.folio.rest.jaxrs.model.Requests;
+import org.folio.rest.jaxrs.model.*;
 import org.folio.rest.jaxrs.resource.IllRaStorage;
 import org.folio.rest.persist.PgUtil;
+import org.folio.util.StringUtil;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
 public class IllRequestsAPI implements IllRaStorage {
+  private static final String ILL_SUBMISSION_TABLE = "ill_submission";
+  private static final String SUBMISSION_STATUS_TABLE = "submission_status";
   private static final String ILL_REQUEST_TABLE = "ill_request";
-  private static final String ILL_STATUS_TABLE = "ill_status";
 
-  @Validate
   @Override
-  public void getIllRaStorageRequests(
-    @Min(0L) @Max(2147483647L) int offset,
-    @Min(0L) @Max(2147483647L) int limit,
-    String query,
-    @Pattern(regexp = "[a-zA-Z]{2}") String lang,
-    Map<String, String> okapiHeaders,
-    Handler<AsyncResult<Response>> asyncResultHandler,
-    Context vertxContext
-  ) {
-    PgUtil.get(
-      ILL_REQUEST_TABLE,
-      Request.class,
-      Requests.class,
-      query,
-      offset,
-      limit,
-      okapiHeaders,
-      vertxContext,
-      GetIllRaStorageRequestsResponse.class,
-      asyncResultHandler
-    );
+  public void getIllRaStorageSubmissions(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.get(ILL_SUBMISSION_TABLE, Submission.class, Submissions.class, query, offset, limit, okapiHeaders, vertxContext, GetIllRaStorageSubmissionsResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void postIllRaStorageRequests(
-    @Pattern(regexp = "[a-zA-Z]{2}") String lang,
-    Request request,
-    Map<String, String> okapiHeaders,
-    Handler<AsyncResult<Response>> asyncResultHandler,
-    Context vertxContext
-  ) {
-    PgUtil.post(
-      ILL_REQUEST_TABLE,
-      request,
-      okapiHeaders,
-      vertxContext,
-      PostIllRaStorageRequestsResponse.class,
-      asyncResultHandler
-    );
+  public void postIllRaStorageSubmissions(String lang, Submission entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.post(ILL_SUBMISSION_TABLE, entity, okapiHeaders, vertxContext, PostIllRaStorageSubmissionsResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void getIllRaStorageRequestsByRequestId(
-    String id,
-    @Pattern(regexp = "[a-zA-Z]{2}") String lang,
-    Map<String, String> okapiHeaders,
-    Handler<AsyncResult<Response>> asyncResultHandler,
-    Context vertxContext
-  ) {
-    PgUtil.getById(
-      ILL_REQUEST_TABLE,
-      Request.class,
-      id,
-      okapiHeaders,
-      vertxContext,
-      GetIllRaStorageRequestsByRequestIdResponse.class,
-      asyncResultHandler
-    );
+  public void getIllRaStorageSubmissionsBySubmissionId(String submissionId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.getById(ILL_SUBMISSION_TABLE, Submission.class, submissionId, okapiHeaders, vertxContext, GetIllRaStorageSubmissionsBySubmissionIdResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void putIllRaStorageRequestsByRequestId(
-    String id,
-    @Pattern(regexp = "[a-zA-Z]{2}") String lang,
-    Request request,
-    Map<String, String> okapiHeaders,
-    Handler<AsyncResult<Response>> asyncResultHandler,
-    Context vertxContext
-  ) {
-    PgUtil.put(
-      ILL_REQUEST_TABLE,
-      request,
-      id,
-      okapiHeaders,
-      vertxContext,
-      PutIllRaStorageRequestsByRequestIdResponse.class,
-      asyncResultHandler
-    );
+  public void putIllRaStorageSubmissionsBySubmissionId(String submissionId, String lang, Submission entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.put(ILL_SUBMISSION_TABLE, entity, submissionId, okapiHeaders, vertxContext, PutIllRaStorageRequestsByRequestIdResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void deleteIllRaStorageRequestsByRequestId(
-    String id,
-    @Pattern(regexp = "[a-zA-Z]{2}") String lang,
-    Map<String, String> okapiHeaders,
-    Handler<AsyncResult<Response>> asyncResultHandler,
-    Context vertxContext
-  ) {
-    PgUtil.deleteById(
-      ILL_REQUEST_TABLE,
-      id,
-      okapiHeaders,
-      vertxContext,
-      DeleteIllRaStorageRequestsByRequestIdResponse.class,
-      asyncResultHandler
-    );
+  public void deleteIllRaStorageSubmissionsBySubmissionId(String submissionId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.deleteById(ILL_SUBMISSION_TABLE, submissionId, okapiHeaders, vertxContext, DeleteIllRaStorageSubmissionsBySubmissionIdResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void getIllRaStorageIllStatuses(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.get(
-      ILL_STATUS_TABLE,
-      IllStatus.class,
-      IllStatuses.class,
-      query,
-      offset,
-      limit,
-      okapiHeaders,
-      vertxContext,
-      GetIllRaStorageIllStatusesResponse.class,
-      asyncResultHandler
-    );
+  public void getIllRaStorageSubmissionsRequestsBySubmissionId(String submissionId, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    try {
+      String submissionQuery = String.format("submissionId == %s", StringUtil.cqlEncode(submissionId));
+      PgUtil.get(ILL_REQUEST_TABLE, Request.class, Requests.class, submissionQuery, offset, limit, okapiHeaders, vertxContext, GetIllRaStorageSubmissionsRequestsBySubmissionIdResponse.class, asyncResultHandler);
+    } catch(Exception e) {
+      asyncResultHandler.handle(Future.succeededFuture(GetIllRaStorageSubmissionsRequestsBySubmissionIdResponse.respond500WithTextPlain(e.getMessage())));
+    }
   }
 
-  @Validate
   @Override
-  public void postIllRaStorageIllStatuses(String lang, IllStatus status, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.post(
-      ILL_STATUS_TABLE,
-      status,
-      okapiHeaders,
-      vertxContext,
-      PostIllRaStorageIllStatusesResponse.class,
-      asyncResultHandler
-    );
+  public void postIllRaStorageSubmissionsRequestsBySubmissionId(String submissionId, String lang, Request entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+
   }
 
-  @Validate
   @Override
-  public void getIllRaStorageIllStatusesByStatusId(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.getById(
-      ILL_STATUS_TABLE,
-      IllStatus.class,
-      id,
-      okapiHeaders,
-      vertxContext,
-      GetIllRaStorageIllStatusesByStatusIdResponse.class,
-      asyncResultHandler
-    );
+  public void getIllRaStorageSubmissionStatuses(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.get(SUBMISSION_STATUS_TABLE, SubmissionStatus.class, SubmissionStatuses.class, query, offset, limit, okapiHeaders, vertxContext, GetIllRaStorageSubmissionStatusesResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void putIllRaStorageIllStatusesByStatusId(String id, String lang, IllStatus status, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.put(
-      ILL_STATUS_TABLE,
-      status,
-      id,
-      okapiHeaders,
-      vertxContext,
-      PutIllRaStorageIllStatusesByStatusIdResponse.class,
-      asyncResultHandler
-    );
+  public void postIllRaStorageSubmissionStatuses(String lang, SubmissionStatus entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.post(SUBMISSION_STATUS_TABLE, entity, okapiHeaders, vertxContext, PostIllRaStorageSubmissionStatusesResponse.class, asyncResultHandler);
   }
 
-  @Validate
   @Override
-  public void deleteIllRaStorageIllStatusesByStatusId(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.deleteById(
-      ILL_STATUS_TABLE,
-      id,
-      okapiHeaders,
-      vertxContext,
-      DeleteIllRaStorageIllStatusesByStatusIdResponse.class,
-      asyncResultHandler
-    );
+  public void getIllRaStorageSubmissionStatusesByStatusId(String statusId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.getById(SUBMISSION_STATUS_TABLE, SubmissionStatus.class, statusId, okapiHeaders, vertxContext, GetIllRaStorageSubmissionStatusesByStatusIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void putIllRaStorageSubmissionStatusesByStatusId(String statusId, String lang, SubmissionStatus entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.put(SUBMISSION_STATUS_TABLE, entity, statusId, okapiHeaders, vertxContext, PutIllRaStorageSubmissionStatusesByStatusIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void deleteIllRaStorageSubmissionStatusesByStatusId(String statusId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.deleteById(SUBMISSION_STATUS_TABLE, statusId, okapiHeaders, vertxContext, DeleteIllRaStorageSubmissionStatusesByStatusIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void getIllRaStorageRequests(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.get(ILL_REQUEST_TABLE, Request.class, Requests.class, query, offset, limit, okapiHeaders, vertxContext, GetIllRaStorageRequestsResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void postIllRaStorageRequests(String lang, Request entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.post(ILL_REQUEST_TABLE, entity, okapiHeaders, vertxContext, PostIllRaStorageRequestsResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void getIllRaStorageRequestsByRequestId(String requestId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.getById(ILL_REQUEST_TABLE, Request.class, requestId, okapiHeaders, vertxContext, GetIllRaStorageRequestsByRequestIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void putIllRaStorageRequestsByRequestId(String requestId, String lang, Request entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.put(ILL_REQUEST_TABLE, entity, requestId, okapiHeaders, vertxContext, PutIllRaStorageRequestsByRequestIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void deleteIllRaStorageRequestsByRequestId(String requestId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.deleteById(ILL_REQUEST_TABLE, requestId, okapiHeaders, vertxContext, DeleteIllRaStorageRequestsByRequestIdResponse.class, asyncResultHandler);
   }
 }
